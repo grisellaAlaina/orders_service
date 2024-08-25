9ae9fa23b73641d52b30992762008b7adc97985f
grisella <serggood@gmail.com>
2024-08-25 20:23:42 +0300

    swagger  contract added

--- src/main/java/org/example/ordersservice/controller/OrdersController.java
+++ src/main/java/org/example/ordersservice/controller/OrdersController.java
@@ -6,6 +6,7 @@ import org.example.ordersservice.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.time.LocalDate;
 import java.util.List;
 
